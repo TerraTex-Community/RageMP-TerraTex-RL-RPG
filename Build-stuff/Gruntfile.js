c9a26b3a79e67ea1ee7f4076da604de40f8b9a55
Geramy92 <admin@terratex.eu>
2018-12-31 17:48:30 +0100

    add some stuff

---
+++ Build-stuff/Gruntfile.js
@@ -0,0 +1,93 @@
+module.exports = function(grunt) {
+    var deployPath =  grunt.option('path') || 'dev';
+
+    grunt.initConfig({
+        'npm-command': {
+            clientpackages_install: {
+                options: {
+                    cmd: 'install',
+                    args: ['--production'],
+                    cwd: '../client-packages'
+                }
+            },
+
+            serverpackages_install: {
+                options: {
+                    cmd: 'install',
+                    args: ['--production'],
+                    cwd: '../server-packages'
+                }
+            },
+
+            ui_install: {
+                options: {
+                    cmd: 'install',
+                    args: ['--production'],
+                    cwd: '../Client-UI'
+                }
+            },
+            clientpackages_build: {
+                options: {
+                    cmd: 'run',
+                    args: ['build'],
+                    cwd: '../client-packages'
+                }
+            },
+            ui_build: {
+                options: {
+                    cmd: 'run',
+                    args: ['build'],
+                    cwd: '../Client-UI'
+                }
+            }
+        },
+        'mkdir': {
+            all: {
+                options: {
+                    mode: 0777,
+                    create: ['dist', 'dist/client_packages', 'dist/client_packages/ui', 'dist/packages']
+                }
+            }
+        },
+        'copy': {
+            ui: {
+                expand: true,
+                src: '**/*',
+                cwd: '../Client-UI/dist/',
+                dest: './dist/client_packages/ui'
+            },
+            client: {
+                expand: true,
+                src: '**/*',
+                dest: './dist/client_packages',
+                cwd: '../client-packages/dist'
+            },
+            server: {
+                expand: true,
+                src: '**/*',
+                dest: './dist/packages/TerraTex',
+                cwd: '../server-packages'
+            },
+            deploy: {
+                expand: true,
+                src: '**/*',
+                dest: deployPath,
+                cwd: './dist'
+            }
+        }
+    });
+
+
+    grunt.loadNpmTasks('grunt-npm-command');
+    grunt.loadNpmTasks('grunt-mkdir');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+
+
+    grunt.registerTask('install', ['npm-command:ui_install','npm-command:serverpackages_install','npm-command:clientpackages_install']);
+    grunt.registerTask('build', ['npm-command:ui_build', 'npm-command:clientpackages_build']);
+    grunt.registerTask('publish', ['mkdir', 'copy:ui', 'copy:client', 'copy:server']);
+
+    grunt.registerTask('default', ['install', 'build', 'publish']);
+    grunt.registerTask('deploy', ['copy:deploy']);
+
+};
