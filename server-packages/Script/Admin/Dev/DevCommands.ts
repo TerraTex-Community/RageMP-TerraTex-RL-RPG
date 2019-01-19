6c06c44ca26ce904bc35d8309cb0698706f825db
Geramy92 <admin@terratex.eu>
2019-01-19 21:07:26 +0100

    test

--- server-packages/Script/Admin/Dev/DevCommands.ts
+++ server-packages/Script/Admin/Dev/DevCommands.ts
@@ -2,6 +2,7 @@ import {DbUser} from '../../../DB/entities/DbUser';
 import Player = RageMP.Player;
 
 mp.events.addCommand("veh", (player: Player, text: string, vehModel: string) => {
-    mp.vehicles.new(mp.joaat(vehModel), player.position);
+    mp.vehicles.new(mp.joaat(vehModel), player.position.add(new mp.Vector3(0, 0, 1)));
 });
 
+mp.events.addCommand("killme", ((player, fullText) => {player.health = 0; }));
--- server-packages/Script/User/Spawn/Death.ts
+++ server-packages/Script/User/Spawn/Death.ts
@@ -4,10 +4,14 @@ import HashOrString = RageMP.HashOrString;
 import {EventHelper} from '../../Helper/EventHelper';
 
 mp.events.add(RageMP.Enums.Event.PLAYER_DEATH, (player: Player, reason: HashOrString, killer: Player) => {
-    const position = getSpawnPosition(player);
 
-    setTimeout(() => {
-        player.spawn(position);
-    }, 10000);
+
+    setTimeout(spawnThePlayer, 10000, player);
 
 });
+
+function spawnThePlayer(player: Player) {
+    console.log("10s later");
+    const position = getSpawnPosition(player);
+    player.spawn(position);
+}
