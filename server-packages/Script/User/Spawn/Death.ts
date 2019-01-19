021876e67d76fa27031543980e3ac4789aa73f20
Geramy92 <admin@terratex.eu>
2019-01-19 21:19:58 +0100

    add fixed respawn

--- server-packages/Script/User/Spawn/Death.ts
+++ server-packages/Script/User/Spawn/Death.ts
@@ -1,17 +1,11 @@
-import {getSpawnPosition} from './Spawn';
+import {spawnPlayer} from './Spawn';
 import Player = RageMP.Player;
 import HashOrString = RageMP.HashOrString;
-import {EventHelper} from '../../Helper/EventHelper';
 
 mp.events.add(RageMP.Enums.Event.PLAYER_DEATH, (player: Player, reason: HashOrString, killer: Player) => {
-
-
     setTimeout(spawnThePlayer, 10000, player);
-
 });
 
 function spawnThePlayer(player: Player) {
-    console.log("10s later");
-    const position = getSpawnPosition(player);
-    player.spawn(position);
+    spawnPlayer(player).catch(e => console.error(e));
 }
