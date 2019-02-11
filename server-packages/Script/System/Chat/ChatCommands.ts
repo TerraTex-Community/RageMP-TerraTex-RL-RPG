ca984ee870761638e162b8cb329966cc68c013f7
Colin Weber <colin.weber@sap.com>
2019-02-11 16:18:27 +0100

    #0000169 add /me

--- server-packages/Script/System/Chat/ChatCommands.ts
+++ server-packages/Script/System/Chat/ChatCommands.ts
@@ -15,3 +15,11 @@ mp.events.addCommand("ooc", ((player, fullText, ...args) => {
 mp.events.addCommand("openchat", (player, fullText, ...args) => {
     player.call("openChatInput", [args.join(" ")]);
 });
+
+mp.events.addCommand("me", ((player, fullText, ...args) => {
+    let sendMessage = new htmlEntities.AllHtmlEntities().encode(args.join(" "));
+    let message = `<i style="color: purple">* ${player.getVariable("customChatNameTag")} ${sendMessage}]]</i>`;
+    mp.players.callInRange(player.position, 20, "addHTML", [message]);
+}));
+
+
