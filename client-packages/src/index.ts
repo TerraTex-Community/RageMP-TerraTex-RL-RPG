7a0cd2be3c40a10635099cf88e119a65b7ce4c9a
Geramy92 <admin@terratex.eu>
2019-04-15 03:26:16 +0200

    add sonar

--- client-packages/src/index.ts
+++ client-packages/src/index.ts
@@ -13,7 +13,6 @@ mp.players.local.alpha = 0;
 mp.players.local.position = new mp.Vector3(0,0,200);
 mp.players.local.dimension = 1;
 
-
 mp.keys.bind(0x71, true, function() {
     mp.gui.cursor.visible = !mp.gui.cursor.visible;
 });
