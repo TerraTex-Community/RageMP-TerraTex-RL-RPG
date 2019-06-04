782dfa4f1883b41e48fe121e9ce57e498280250a
Geramy92 <admin@terratex.eu>
2019-06-04 16:46:05 +0200

    add Vehicle Price Check #138

--- server-packages/Lib/Data/VehicleWikiCheck.ts
+++ server-packages/Lib/Data/VehicleWikiCheck.ts
@@ -1,8 +1,9 @@
 import {logger} from "../Services/logging/logger";
 import {getVehicleListItemByName} from "../../Script/System/Vehicle/VehicleList";
-import puppeteer from 'puppeteer';
+import puppeteer from "puppeteer";
 import {waitForBrowserTitleToContain} from "../../Script/Helper/PuppeteerHelper";
 import {LOG_TYPES} from "../Services/logging/log_types";
+import {VEHICLE_BUY_MODE, VehicleListItem} from "../../Script/System/Vehicle/VehicleListItem";
 
 const wikiUrl = "https://wiki.rage.mp/index.php?title=Vehicles";
 
@@ -15,16 +16,27 @@ export async function checkVehicleListAgainsWiki(): Promise<void> {
         const contents = await page.$$(".gallerybox .gallerytext code");
 
         const listOfMissingVehicles: string[] = [];
+        const listOfMissingVehiclePrices: string[] = [];
         for (const element of contents) {
             const name = (await (await element.getProperty("textContent")).jsonValue()).trim();
-            if(!getVehicleListItemByName(name)) {
+            const vehicleItem: VehicleListItem|false = getVehicleListItemByName(name);
+            if(!vehicleItem) {
                 listOfMissingVehicles.push(name);
+            } else if (vehicleItem.price === -1 && vehicleItem.buyMode !== VEHICLE_BUY_MODE.NOT_BUYABLE) {
+                listOfMissingVehiclePrices.push(name);
             }
         }
 
         if (listOfMissingVehicles.length > 0) {
             logger.warn("Vehicles in List (compared to Wiki) are Missing", {
                 missingItems: listOfMissingVehicles,
+                type: LOG_TYPES.MISSING_ITEMS
+            });
+        }
+
+        if (listOfMissingVehiclePrices.length > 0) {
+            logger.warn("Vehicles in List that are buyable have no price", {
+                missingItems: listOfMissingVehiclePrices,
                 type: LOG_TYPES.MISSING_ITEMS
             });
         }
--- server-packages/Lib/Lists/last_updated.md
+++ server-packages/Lib/Lists/last_updated.md
@@ -1,10 +1,6 @@
 Update definitions:
 - run script ```npm run generate_list_definitions```
-
-Dates of files:
-- **vehicles.json:**   
-    date: 26.04.2019   
-    link: https://wiki.rage.mp/index.php?title=Vehicles    
+  
 - **weapons.json:**   
     date: 26.04.2019   
     link: https://wiki.rage.mp/index.php?title=Weapons   
