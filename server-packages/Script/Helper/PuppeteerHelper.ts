036c0fae332f25014224b63beeebc3c66dc5291e
Geramy92 <admin@terratex.eu>
2019-06-05 17:03:56 +0200

    retry in case of useless error

--- server-packages/Script/Helper/PuppeteerHelper.ts
+++ server-packages/Script/Helper/PuppeteerHelper.ts
@@ -3,9 +3,16 @@ import {wait} from "./Utilities";
 
 export async function waitForBrowserTitleToContain(page: Page, content: string, maxTimeOut: number = 300000): Promise<void> {
     let calcTime = 0;
-    while ((await page.title()).toLowerCase().indexOf(content.toLowerCase()) === -1) {
-        const title = await page.title();
-        if (calcTime >= maxTimeOut) throw new Error(`waitForBrowserTitle timed out. ${title} != ${content}`);
+    let actualTitle = await page.title();
+    while (actualTitle.toLowerCase().indexOf(content.toLowerCase()) === -1) {
+        try {
+            actualTitle = await page.title();
+        } catch (e) {
+            console.error("retry because of error");
+        }
+        if (calcTime >= maxTimeOut) {
+            throw new Error(`waitForBrowserTitle timed out. ${actualTitle} != ${content}`);
+        }
         await wait(300);
         calcTime += 300;
     }
