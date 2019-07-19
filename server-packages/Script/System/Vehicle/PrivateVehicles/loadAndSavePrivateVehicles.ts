8c37942e7a10965bb909ffc6cbdff5d527845d42
Geramy92 <admin@terratex.eu>
2019-07-19 14:49:46 +0200

    remove not parked vehicles

--- server-packages/Script/System/Vehicle/PrivateVehicles/loadAndSavePrivateVehicles.ts
+++ server-packages/Script/System/Vehicle/PrivateVehicles/loadAndSavePrivateVehicles.ts
@@ -2,13 +2,23 @@ import {DbUserVehicle} from "../../../../DB/entities/DbUserVehicle";
 import {PrivateVehicle, privateVehicles} from "./PrivateVehicle";
 import {ShutdownService} from "../../../../Lib/Services/ShutdownService";
 import addToShutdownService = ShutdownService.addToShutdownService;
+import {getVehicleListItemByName, VEHICLE_LIST} from "../VehicleList";
 
 export async function loadAllPrivateVehicle(): Promise<void> {
     const allPrivateVehicles = await DbUserVehicle.find();
 
     for (const pVeh of allPrivateVehicles) {
-        // tslint:disable-next-line:no-unused-expression
-        new PrivateVehicle(pVeh.owner, pVeh);
+        if (pVeh.positionData.x === 0 && pVeh.positionData.y === 0 && pVeh.positionData.z === 0) {
+            const item = getVehicleListItemByName(pVeh.model);
+            if (item) {
+                pVeh.owner.inventory.money += item.price * 0.8;
+                await pVeh.owner.save();
+            }
+            pVeh.remove();
+        } else {
+            // tslint:disable-next-line:no-unused-expression
+            new PrivateVehicle(pVeh.owner, pVeh);
+        }
     }
 }
 
