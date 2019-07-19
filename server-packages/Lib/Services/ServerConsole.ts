b68d994acf48a920ddaae2013d2fc785d8139af4
Geramy92 <admin@terratex.eu>
2019-07-19 04:26:56 +0200

    #148 add Serverconsole command parsing

--- server-packages/Lib/Services/ServerConsole.ts
+++ server-packages/Lib/Services/ServerConsole.ts
@@ -1,5 +1,8 @@
 import readline from "readline";
 import {Chat} from "../../Script/System/Chat/Chat";
+import {logger} from "./logging/logger";
+
+const serverCmds: {[cmd: string]: Function} = {};
 
 const rl = readline.createInterface({
     input: process.stdin,
@@ -8,12 +11,29 @@ const rl = readline.createInterface({
 
 function parseInput(s: string): void {
     if (s.startsWith("/")) {
-        // @todo parse cmd
+        let parts = s.split(" ");
+        const cmd = parts[0].substring(1);
+
+        if (serverCmds[cmd]) {
+            parts = parts.slice(1);
+            serverCmds[cmd].apply(null, parts);
+            logger.info("Sended Serverconsole Command: ", {cmd, args: parts});
+        } else {
+            console.error(`Cmd ${cmd} doesn't exist for serverconsole.`);
+        }
+
     } else {
         Chat.sendChatNotificationToPlayer(mp.players.toArray(), s, "Servernachricht");
+        logger.info("Sended Serverconsole Message: " + s);
     }
 }
 
 rl.on('line', (s) => {
     parseInput(s);
 });
+
+export function registerServerCommand(cmd: string, func: Function): void {
+    if (serverCmds[cmd]) throw new Error(`Cmd ${cmd} already exist`);
+
+    serverCmds[cmd] = func;
+}
