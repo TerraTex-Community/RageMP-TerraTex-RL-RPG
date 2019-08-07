8052b0e3eaefdffcfe0e633c10a1fc6bf9ff34ad
Geramy92 <admin@terratex.eu>
2019-08-07 15:02:36 +0200

    sonar fixes

--- client-packages/src/ClientConsole/Logger.ts
+++ client-packages/src/ClientConsole/Logger.ts
@@ -1,27 +1,27 @@
 import {printToConsole} from "./ClientConsoleHandler";
 
 class Logger {
-    log(msg: string, data: object = {}) {
+    log(msg: string, data: object = {}): void {
         this.sendLogToServer("log", msg, data);
     }
 
-    warn(msg: string, data: object = {}) {
+    warn(msg: string, data: object = {}): void {
         this.sendLogToServer("warn", msg, data);
     }
 
-    info(msg: string, data: object = {}) {
+    info(msg: string, data: object = {}): void {
         this.sendLogToServer("info", msg, data);
     }
 
-    debug(msg: string, data: object = {}) {
+    debug(msg: string, data: object = {}): void {
         this.sendLogToServer("debug", msg, data);
     }
 
-    error(msg: string, data: object = {}) {
+    error(msg: string, data: object = {}): void {
         this.sendLogToServer("error", msg, data);
     }
 
-    crit(msg: string, data: object = {}) {
+    crit(msg: string, data: object = {}): void {
         this.sendLogToServer("crit", msg, data);
     }
 
@@ -29,12 +29,12 @@ class Logger {
         mp.events.add("log_server_to_client", this.receiveConsoleLog);
     }
 
-    private receiveConsoleLog(logType: string, dataMsg: string) {
+    private receiveConsoleLog(logType: string, dataMsg: string): void {
         printToConsole("info", "received following Log from Server");
         printToConsole(logType, dataMsg);
     }
 
-    private sendLogToServer(logType: string, msg: string, data: object) {
+    private sendLogToServer(logType: string, msg: string, data: object): void {
         mp.events.callRemote("log_client_to_server", JSON.stringify({
             type: logType,
             message: msg,
