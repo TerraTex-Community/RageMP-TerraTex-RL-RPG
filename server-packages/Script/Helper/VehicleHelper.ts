aa279b256d67f7943d3150711097fe1e87e97f9b
geramy92 <info@geramy92.de>
2020-04-09 15:10:11 +0200

    test warp in vehicle without timeout

--- server-packages/Script/Helper/VehicleHelper.ts
+++ server-packages/Script/Helper/VehicleHelper.ts
@@ -6,7 +6,7 @@ import {logger} from "../../Lib/Services/logging/logger";
 export class VehicleHelper {
 
     static async ensurePlayerInVehicle(player: Player, vehicle: Vehicle, seat: number = 0): Promise<void> {
-        await wait(1000);
+        // await wait(1000);
         while (player.vehicle !== vehicle) {
             player.putIntoVehicle(vehicle, seat);
             await wait(200);
